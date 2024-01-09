8b723be94f24d8b78c82a25f6d14fe15dc7f8cc6
Rhythm <rhythmj45@gmail.com>
2024-01-09 23:59:38 +0530

    test

---
+++ src/SeleniumIntro.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.chrome.driver", "/Users/rhythmj45/Downloads/chromedriver_mac_arm64");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.youtube.com/");
+		System.out.print(driver.getTitle());
+		driver.close();	}
+
+}
